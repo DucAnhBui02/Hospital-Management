4c58bfa4b3f1b6218b487d086210b5183ecdb267
DucAnhBui02 <87839908+DucAnhBui02@users.noreply.github.com>
2024-03-22 15:02:40 +0700

    get all data users

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -18,6 +18,24 @@ let handleLogin = async (req, res) => {
   });
 };
 
+let handleGetAllUsers = async (req, res) => {
+  let id = req.query.id;
+
+  if (!id) {
+    return res.status(200).json({
+      errCode: 1,
+      errMessage: "Missing required parameter",
+    });
+  }
+  let users = await userService.getAllUsers(id);
+
+  return res.status(200).json({
+    errCode: 0,
+    errMessage: "Ok!",
+    users: users,
+  });
+};
 module.exports = {
   handleLogin: handleLogin,
+  handleGetAllUsers: handleGetAllUsers,
 };
